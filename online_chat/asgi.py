efe060727d25749b01b01d5ee96365c5f0b6b4b2
Ahmed Yasser <ahmedahmedyasseryasser@gmail.com>
2021-07-20 19:21:37 +0200

    the setup in asgi

--- online_chat/asgi.py
+++ online_chat/asgi.py
@@ -8,18 +8,20 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
-from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.routing import ProtocolTypeRouter, URLRouter,  get_default_application
 from channels.auth import AuthMiddlewareStack
+
 from django.core.asgi import get_asgi_application
 from main.routing import websocket_urlpatterns
 from django import setup
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'online_chat.settings')
-
+setup()
 django_asgi_app = get_asgi_application()
 
-application = ProtocolTypeRouter({
-    "http": django_asgi_app,
-    "websocket": AuthMiddlewareStack(
-        URLRouter(websocket_urlpatterns)
-    )
-})
+# application = ProtocolTypeRouter({
+#     "http": django_asgi_app,
+#     "websocket": AuthMiddlewareStack(
+#         URLRouter(websocket_urlpatterns)
+#     )
+# })
+application = get_default_application()
