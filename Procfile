e22e2ca1e879f54ac7b04a80ca8d8eaa2a1fa2fb
Ahmed Yasser <ahmedahmedyasseryasser@gmail.com>
2021-07-20 20:04:37 +0200

    ewewewf

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
 release: python manage.py migrate
-web: daphne online_chat.asgi:application --port $PORT
-chatworker: python manage.py runworker --settings=online_chat.settings
+web: daphne online_chat.asgi:application --port $PORT --bind 0.0.0.0
+worker: python manage.py runworker channel_layer --settings=online_chat.settings
--- online_chat/asgi.py
+++ online_chat/asgi.py
@@ -8,16 +8,17 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
-from channels.routing import ProtocolTypeRouter, URLRouter,  get_default_application
+import django
+from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-
 from django.core.asgi import get_asgi_application
 from main.routing import websocket_urlpatterns
-
+django.setup()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'online_chat.settings')
 
 django_asgi_app = get_asgi_application()
+ASGI_APPLICATION = django_asgi_app
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
