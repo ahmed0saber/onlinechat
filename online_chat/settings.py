7dc26101bf2ada3472a96a5a9f87290399dfa8f3
Ahmed Yasser <ahmedahmedyasseryasser@gmail.com>
2021-07-22 05:04:47 +0200

    removed empty files and debug false

--- online_chat/settings.py
+++ online_chat/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", config("SECRET_KEY"))
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["192.168.1.10", "127.0.0.1", 'globalonlinechat.herokuapp.com']
 
