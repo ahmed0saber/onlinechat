23ab38c2d5ad65e0d50010e4066f28e40498206e
Ahmed Yasser <ahmedahmedyasseryasser@gmail.com>
2021-07-22 04:05:49 +0200

    async unsafe

--- online_chat/asgi.py
+++ online_chat/asgi.py
@@ -16,10 +16,11 @@ from main.routing import websocket_urlpatterns
 from .wsgi import *
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'online_chat.settings')
+os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 django.setup()
 # application = get_default_application()
 django_asgi_app = get_asgi_application()
-ASGI_APPLICATION = get_asgi_application()
+# ASGI_APPLICATION = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
