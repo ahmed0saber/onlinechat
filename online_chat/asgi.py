b8e9fbb42d15bd4e22d206da30d05ecebe18787b
Ahmed Yasser <ahmedahmedyasseryasser@gmail.com>
2021-07-22 04:55:35 +0200

    removed wsgi from asgi

--- online_chat/asgi.py
+++ online_chat/asgi.py
@@ -10,14 +10,12 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 import os
 import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'online_chat.settings')
-# os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 django.setup()
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
 from main.routing import websocket_urlpatterns
-from .wsgi import *
 
 django_asgi_app = get_asgi_application()
 
