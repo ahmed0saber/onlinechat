99c51ecd693c46c700e6dd7ec2f58e983eafe190
Ahmed Yasser <ahmedahmedyasseryasser@gmail.com>
2021-07-22 04:11:41 +0200

    rearrange

--- online_chat/asgi.py
+++ online_chat/asgi.py
@@ -9,15 +9,15 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 
 import os
 import django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'online_chat.settings')
+os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
+django.setup()
 from channels.routing import ProtocolTypeRouter, URLRouter, get_default_application
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
 from main.routing import websocket_urlpatterns
 from .wsgi import *
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'online_chat.settings')
-os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
-django.setup()
 # application = get_default_application()
 django_asgi_app = get_asgi_application()
 # ASGI_APPLICATION = get_asgi_application()
