713e595a71c2ca5a0c1c9de06e8737822b369c04
Ahmed Yasser <ahmedahmedyasseryasser@gmail.com>
2021-08-13 18:13:03 +0200

    normal path not repath

--- main/routing.py
+++ main/routing.py
@@ -2,6 +2,6 @@ from django.urls import re_path, path
 from .consumers import ChatConsumer
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/', ChatConsumer.as_asgi()),
-    # path('ws/chat/', ChatConsumer.as_asgi())
+    # re_path(r'ws/chat/', ChatConsumer.as_asgi()),
+    path('ws/chat/', ChatConsumer.as_asgi())
 ]
